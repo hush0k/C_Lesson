bd2e3ccb40c587d620958c11bb18d803c7969e0a
Kanysh <127661635+hush0k@users.noreply.github.com>
2025-05-07 11:14:33 +0500

    Add files via upload

---
+++ Ilayas/SecondLesson.cpp
@@ -0,0 +1,67 @@
+#include <iomanip>
+#include <iostream>
+#include <string>
+
+
+
+int main() {
+    //Name of data
+    //string 84name; ERROR
+    //return, int, string, or
+    //int return ERROR
+
+    //Всегда есть значение
+    // string name,
+    // string nameOfCompany
+    // string name_of_Company
+
+
+    // //Целые числа
+    // int myNumber = -48;
+    // cout << "The number is " << myNumber << endl;
+    //
+    // //Беззнаковые целые числа
+    // unsigned int noSigNum = 123456;
+    // cout << "The number is " << noSigNum << endl;
+    //
+    // //Длинные числа
+    // long longNum = 161844984;
+    // cout << "The number is " << longNum << endl;
+    //
+    // //ОООЧЕНЬ БОЛЬШИЕ ЧИСЛА
+    // long long veryLongNum = 161844984948484817;
+    // cout << "The number is " << veryLongNum << endl;
+    //
+    //Цифры с плавающими точками
+    float pi = 3.14159;
+    std::cout << "The pi is " << pi << std::endl;
+
+    //Большие числы с точками
+    double pi2 = 3.141592653589793;
+    std::cout << "The pi is " << pi2 << std::endl;
+
+    std::cout << std::fixed << std::setprecision(3);
+    std::cout << "The pi is " << pi << std::endl;
+
+    // //Символ
+    // char c = '!';
+    // cout << "The character is " << c << endl;
+    //
+    // cout << "char is " << c << " (ASCII code: " << (int)c << ")" << endl;
+    //
+    // //Логические значение
+    // bool yes = true;
+    // bool no = false;
+    //
+    // cout << "YES is " << yes << endl;
+    // cout << "NO is " << no << endl;
+    //
+    // //Строки
+    // string yesStr = "yes";
+    // cout << "YES length is: " << yesStr << endl;
+
+
+
+
+
+}
---
+++ Ilayas/firstLesson.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main() {
+    /*//Print command
+    std::cout << "Hello World" << std::endl;*/
+
+
+    /*//Input command
+    int a;
+    std::cin >> a;
+    std::cout << a << std::endl;*/
+
+    // std::string name;
+    // std::cout << "Enter your name and surname: ";
+    // std::cin >> name;
+    //
+    // std::cout << "Your name and surname is: " << name << std::endl;
+
+    //getline
+    /*std::string text;
+    std::cout << "Enter your text: ";
+    std::getline(std::cin, text);
+    std::cout << "Your text: " << text << std::endl;*/
+
+    int a = 10;
+    int b = 20;
+    std::cout << a + b << std::endl;
+
+}
